c23a0125c02c8472a2c8a81dc38e34e746d7f49e
Rgraylemon <hktstsi77@gmail.com>
2023-07-09 14:26:01 +0900

    update: change page after sound end

--- Android/WebViewSound/app/src/main/assets/script/page1.js
+++ Android/WebViewSound/app/src/main/assets/script/page1.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', ()=>{
+    let isChangePage = false;
 
     // 画面クリックで音を鳴らす
     document.body.addEventListener("click", function(){
@@ -7,7 +8,14 @@ window.addEventListener('load', ()=>{
         audio.play();
     });
 
+    // 音声再生が終わってから画面遷移させる
+    document.getElementById("audio").addEventListener("ended", function(){
+        if(isChangePage){
+            location.href = '../html/page2.html';
+        }
+    });
+
     document.getElementById("movePage2Button").addEventListener("click", function(){
-        location.href = '../html/page2.html';
+        isChangePage = true;
     })
 });
